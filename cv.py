47fbe25badce22510b4fe2dfb32249b79d9fee60
Dhanush R <44114205+Dhanushadoor@users.noreply.github.com>
2020-01-29 19:12:26 +0530

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,8 @@
+import cv2
+img = cv2.imread("pic.jpg",1)
+print(img.shape)
+# cv2.imshow("Dhanush",img)
+res = cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
+cv2.imshow("Dhanushhhhhhhhhh",res)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ imageDetection.py
@@ -0,0 +1,31 @@
+import cv2
+
+#create the haar cascade ---- creating the classifier
+clasi = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+# import the image
+image = cv2.imread("niri.jpg",1)
+i=2
+r_image = cv2.resize(image,(int(image.shape[1]/i),int(image.shape[0]/i)))
+
+#Detecting the faces in the image
+face= clasi.detectMultiScale(
+    r_image,
+    scaleFactor=1.3,
+    minNeighbors=2)
+
+# printing that face is found
+print("Pooja is detected {0}".format(len(face)))
+print(type(face))
+print(face)
+
+
+# drawing a rectangle
+for (x,y,w,h) in face :
+    cv2.rectangle(r_image,(x,y),(x+w,y+h),(100,10,255),2)
+
+
+#Displaying
+cv2.imshow("Pooja Hegde",r_image)
+cv2.waitKey(0)
+
